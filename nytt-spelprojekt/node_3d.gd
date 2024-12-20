0be9aa419aebad014e76350f503c247180842722
Tobias Svensson <tobsv650@student.liu.se>
2024-12-20 13:24:07 +0100

    Lade till 3d rörelser samt generering av labyrint

---
+++ nytt-spelprojekt/node_3d.gd
@@ -0,0 +1,83 @@
+extends Node3D
+
+#var maze = [
+	#[1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	#[1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+	#[1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+	#[1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+	#[1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+	#[1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+	#[1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+	#[1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+	#[1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+	#[1, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+#]
+var newChunkLoaded = false
+
+var maze = [
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+	[1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+	[1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+	[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+	[1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1],
+	[1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+	[1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+	[1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1],
+	[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1],
+	[1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+	[1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1],
+	[1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+var hedge = preload("res://hedge.tscn")
+
+func loadChunk(newCenter):
+	
+	for z in range(maze.size()):  # Loop over rows (z is the index for rows)
+		for x in range(maze[z].size()):  # Loop over columns (x is the index for columns)
+			if maze[z][x] == 1:
+				# Create new instance at position (x, 0, z)
+				var pos = Vector3((x-10), 1, (z+10))
+				inst(pos) 
+	return
+	
+	return
+
+func inst(pos):
+	var instance = hedge.instantiate()
+	instance.position = pos
+	add_child(instance)
+	return
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	for z in range(maze.size()):  # Loop over rows (z is the index for rows)
+		for x in range(maze[z].size()):  # Loop over columns (x is the index for columns)
+			if maze[z][x] == 1:
+				# Create new instance at position (x, 0, z)
+				var pos = Vector3((x-10), 1, (z-10))
+				inst(pos) 
+	return
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var charachter = $CharacterBody3D
+	var pos = Vector3(charachter.position)
+	
+	if pos.z > 8 && !newChunkLoaded:
+		#print("pos över 10")
+		loadChunk(pos)
+		newChunkLoaded = true
+	return
+	
+	
